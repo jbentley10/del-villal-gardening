dfa0d50f678090cb043fa58afd4c259745efccdf
John Bentley <jwbentle@gmail.com>
2025-01-19 18:49:02 -0800

    Update homepage meta description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ import Content from "./content";
 
 // Set metadata
 export const metadata = {
-  title: "Home | Example app",
-  description: "This app uses NextJS and Contentful.",
+  title: "Home | Del Villal Gardening",
+  description: "Professional landscaping and garden maintenance services for residential and commercial properties. From irrigation and lighting to tree care and regular upkeep. Licensed and insured.",
 };
 
 export default async function Home() {
