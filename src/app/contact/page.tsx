7261031c28a184ada57ab4d236f69896a0b8c541
John Bentley <jwbentle@gmail.com>
2025-01-19 18:58:43 -0800

    Scaffold out remaining pages

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,33 @@
+/**
+ * @file page.tsx
+ */
+// Import components and utils
+import { fetchBlocksBySlug } from "../../lib/contentfulData";
+import Content from "../content";
+
+// Set metadata
+export const metadata = {
+  title: "Home | Del Villal Gardening",
+  description: "Professional landscaping and garden maintenance services for residential and commercial properties. From irrigation and lighting to tree care and regular upkeep. Licensed and insured.",
+};
+
+export default async function Contact() {
+  const blocksEnglish = await fetchBlocksBySlug("contact", "en-US");
+  const blocksSpanish = await fetchBlocksBySlug("contact", "es");
+
+  // Wait for the promises to resolve
+  const [english, spanish] = await Promise.all([blocksEnglish, blocksSpanish]);
+
+  return (
+    <main>
+      {english && spanish && (
+        <Content
+          key={Math.random()}
+          englishBlocks={english}
+          spanishBlocks={spanish}
+        />
+      )}
+      ;
+    </main>
+  );
+}
